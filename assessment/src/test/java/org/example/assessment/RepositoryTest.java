0de3c8189ec3e23abb2f51908d46b95d35b247b5
rslvn <avan.resul@gmail.com>
2018-05-03 17:14:41 +0300

    Step 3: add hippo-query repository print

--- assessment/src/test/java/org/example/assessment/RepositoryTest.java
+++ assessment/src/test/java/org/example/assessment/RepositoryTest.java
@@ -1,32 +1,138 @@
 package org.example.assessment;
 
-import javax.jcr.Node;
-import javax.jcr.RepositoryException;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import javax.jcr.*;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.Test;
 import org.onehippo.repository.testutils.RepositoryTestCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import org.hippoecm.repository.util.Utilities;
-
-import static org.junit.Assert.assertTrue;
+import com.google.common.collect.Lists;
 
 public class RepositoryTest extends RepositoryTestCase {
 
-    @Test
-    public void test_minimal() throws RepositoryException {
-        Node rootNode = session.getRootNode();
-        rootNode.addNode("test");
-        rootNode.addNode("books");
-        session.save();
-        assertTrue(session.nodeExists("/test"));
-        assertTrue(session.nodeExists("/books"));
-    }
+	private final static String HIPPO_QUERY_PATH = "/hippo:configuration/hippo:queries";
+	private final static String PROPRTY_VALUE_FORMATTER = "%s= %s";
 
-    @After
-    @Override
-    public void tearDown() throws Exception {
-        removeNode("/books");
-        super.tearDown(); // removes /test node and checks repository clean state
-    }
+	@Test
+	public void test_minimal() throws RepositoryException {
+		Node rootNode = session.getRootNode();
+		rootNode.addNode("test");
+		rootNode.addNode("books");
+		session.save();
+		assertTrue(session.nodeExists("/test"));
+		assertTrue(session.nodeExists("/books"));
+	}
+
+	@After
+	@Override
+	public void tearDown() throws Exception {
+		removeNode("/books");
+		super.tearDown(); // removes /test node and checks repository clean
+							// state
+	}
+
+	@Test
+	public void testPrintHippoQueries() throws RepositoryException, IOException {
+		log.info(toString(session.getNode(HIPPO_QUERY_PATH)));
+	}
+
+	/**
+	 * Traverses and prints nodes
+	 *
+	 * @param node
+	 * @throws RepositoryException
+	 */
+	private String toString(Node node) throws RepositoryException, IOException {
+
+		assertNotNull(node);
+
+		StringBuilder sb = new StringBuilder();
+		if (node.getParent() != null) {
+			sb.append("\n");
+		}
+		sb.append(printableNodeText(node));
+
+		// toString subNodes
+		NodeIterator nodeIterator = node.getNodes();
+		while (nodeIterator.hasNext()) {
+			sb.append(toString(nodeIterator.nextNode()));
+		}
+
+		return sb.toString();
+	}
+
+	/**
+	 * Prints path, type and values of a node
+	 * 
+	 * @param node
+	 * @throws RepositoryException
+	 */
+	public String printableNodeText(Node node) throws RepositoryException, IOException {
+
+		StringBuilder sb = new StringBuilder();
+
+		String pathAsString = String.format(PROPRTY_VALUE_FORMATTER, "NODE", node.getName());
+		sb.append(StringUtils.leftPad(pathAsString, pathAsString.length() + node.getDepth())).append("\n");
+
+		pathAsString = String.format(PROPRTY_VALUE_FORMATTER, "IDENTIFIER", node.getIdentifier());
+		sb.append(StringUtils.leftPad(pathAsString, pathAsString.length() + node.getDepth())).append("\n");
+
+		pathAsString = String.format(PROPRTY_VALUE_FORMATTER, "PATH", node.getPath());
+		sb.append(StringUtils.leftPad(pathAsString, pathAsString.length() + node.getDepth())).append("\n");
+
+		sb.append(StringUtils.leftPad("Properties= ", "Properties= ".length() + node.getDepth())).append("\n");
+
+		PropertyIterator propertyIterator = node.getProperties();
+		while (propertyIterator.hasNext()) {
+			sb.append(toString(propertyIterator.nextProperty(), node.getDepth() + 1)).append("\n");
+		}
+
+		return sb.toString();
+	}
+
+	/**
+	 * @param property
+	 * @param deep
+	 * @return
+	 * @throws RepositoryException
+	 */
+	private String toString(Property property, int deep) throws RepositoryException, IllegalStateException, IOException {
+		String propertyValue;
+		if (property.isMultiple()) {
+			propertyValue = Arrays.toString(Lists.newArrayList(property.getValues()).parallelStream().map(v -> {
+				try {
+					return v.getString();
+				} catch (ValueFormatException e) {
+					e.printStackTrace();
+				} catch (IllegalStateException e) {
+					e.printStackTrace();
+				} catch (RepositoryException e) {
+					e.printStackTrace();
+				}
+				return "";
+			}).collect(Collectors.toList()).toArray());
+		} else if(property.getType() == PropertyType.BINARY){
+			propertyValue = property.getBinary().toString();
+		} else{
+			propertyValue = property.getValue().getString();
+
+		}
+
+		String text = String.format(PROPRTY_VALUE_FORMATTER, property.getName(), propertyValue);
+
+		return StringUtils.leftPad(text, text.length() + deep);
+	}
 }
